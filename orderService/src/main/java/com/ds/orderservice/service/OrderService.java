83b282d5d885e9b271f6222526f9113f63876450
j_sandeera <sandeerads@gmail.com>
2023-02-25 18:48:32 +0530

    add centralize config

--- orderService/src/main/java/com/ds/orderservice/service/OrderService.java
+++ orderService/src/main/java/com/ds/orderservice/service/OrderService.java
@@ -5,28 +5,28 @@ import com.ds.orderservice.common.TransactionRequest;
 import com.ds.orderservice.common.TransactionResponse;
 import com.ds.orderservice.entity.Order;
 import com.ds.orderservice.repository.OrderRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     Logger logger = LoggerFactory.getLogger(OrderService.class);
     @Autowired
     private OrderRepository repository;
     @Autowired
-//    @Lazy
+   @Lazy
     private RestTemplate template;
 
-//    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
-//    private String ENDPOINT_URL;
+    @Value("${microServices.paymentService.endPoints.endPoint1.url}")
+    private String PAYMENT_SERVICE_ENDPOINT_URL_01;
 
     public TransactionResponse saveOrder(TransactionRequest request) {
         String response = "";
@@ -35,11 +35,9 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         // rest call
-//      logger.info("Order-Service Request : "+new ObjectMapper().writeValueAsString(request));
-        Payment paymentResponse = template.postForObject("http://PAYMENTSERVICE/payment/doPayment", payment, Payment.class);
+        Payment paymentResponse = template.postForObject(PAYMENT_SERVICE_ENDPOINT_URL_01, payment, Payment.class);
 //
         response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successful and order placed" : "there is a failure in payment api , order added to cart";
-//        logger.info("Order Service getting Response from Payment-Service : " + new ObjectMapper().writeValueAsString(response));
         repository.save(order);
         return new TransactionResponse(order, paymentResponse.getAmount(), paymentResponse.getTransactionId(), response);
     }
